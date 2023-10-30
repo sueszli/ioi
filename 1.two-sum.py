42a4511b99d52fde268d8342e581bfd355e0b8e4
Yahya Jabary <jabaryyahya@gmail.com>
2023-10-29 21:20:38 -0400

    update

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -3,18 +3,19 @@ from typing import List
 def two_sum(nums: List[int], target: int) -> List[int]:
     result = [-1, -1]
 
-    # there are len(nums)*2 unique combinations as (i+j) == (j,i)
-    checked = [False for i in range(len(nums) * 2 - 1)]
+    # there are len(nums)*2 unique combinations as (i+j) == (j+i)
+    checked = [False for i in range(len(nums) * 2 - 1)]    
 
-    for i in nums:
-        for j in nums:
-            if (checked[i+j] == False) and (i + j == target):
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if (checked[i+j] == False) and (nums[i] + nums[j] == target):
                 return [i,j]
             checked[i+j] = True
+    
     return result
 
 if __name__ == "__main__":
-    nums = [0,1,2,3,4,5,6,7,8,9]
-    target = 10
+    nums = [2,7,11,15]
+    target = 9
     result = two_sum(nums, target)
     print(f'{nums} with target {target} --> {result}')
