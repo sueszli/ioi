f80e4053f5704d193a23eb8fe4da4eda3ec919f0
Yahya Jabary <jabaryyahya@gmail.com>
2023-10-31 14:52:15 -0400

    update

--- 9.palindrome-number.py
+++ 9.palindrome-number.py
@@ -9,24 +9,23 @@ def is_palindrome_simple(x: int) -> bool:
     return True
 
 
+def is_palindrome_simple_v2(x: int) -> bool:
+    return str(x) == str(x)[::-1]
+
+
 def is_palindrome(x: int) -> bool:
-    len: int = math.ceil(math.log(num, 10))
-    half_len: int = math.floor(len / 2.0)
+    reversed = 0
+    copy = x
 
-    for i in range(half_len):
-        # right_removed = int(x / int(math.pow(10, i)))
-        # left_removed = right_removed % int(math.pow(10, 2 * half_len - i))
-        leftmost = x % 10
-        # rightmost = int(x / int(math.pow(10, 2 * half_len - i)))
+    while copy != 0:
+        digit = copy % 10
+        reversed = reversed * 10 + digit
+        copy //= 10
 
-        x = int(x / 10)
-
-        print(leftmost)
-
-    return False
+    return reversed == x
 
 
 if __name__ == "__main__":
     num = 1234321
-    result = is_palindrome_simple(num)
+    result = is_palindrome(num)
     print(f"{num} --> {result}")
