e2a0cb4c1210b12f97afab5db0eac065b57d5020
Yahya Jabary <jabaryyahya@gmail.com>
2023-11-04 06:32:44 -0400

    update

--- kata.fusc.js
+++ kata.fusc.js
@@ -1,7 +1,21 @@
 // https://www.codewars.com/kata/570409d3d80ec699af001bf9
+
 /*
-1. fusc(0) = 0
-2. fusc(1) = 1
-3. fusc(2 * n) = fusc(n)
-4. fusc(2 * n + 1) = fusc(n) + fusc(n + 1)
+fusc(0) = 0
+fusc(1) = 1
+fusc(2n) = fusc(n)
+fusc(2n + 1) = fusc(n) + fusc(n + 1)
 */
+
+function fusc(n) {
+    if (n == 0 || n == 1) {
+        return n
+    }
+    if (n % 2 == 0) {
+        return fusc(n / 2)
+    } else {
+        return fusc((n - 1) / 2) + fusc((n + 1) / 2)
+    }
+}
+
+console.log(fusc(10))
