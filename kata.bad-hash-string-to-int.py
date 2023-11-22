537846b4eee98356cc9a94f7bc4ee92ada9d9b8e
Yahya Jabary <jabaryyahya@gmail.com>
2023-11-22 16:04:41 -0500

    update

--- kata.bad-hash-string-to-int.py
+++ kata.bad-hash-string-to-int.py
@@ -1,7 +1,12 @@
+# https://www.codewars.com/kata/596d93bd9b6a5df4de000049
+
+
 def hash_str(string: str) -> int:
     a: int = sum([ord(char) for char in string])  # char sum
     get_prev_diff = lambda string, i: ord(string[i - 1]) - ord(string[i]) if i != 0 else 0  # diff with previous char
-    c: int = sum([get_prev_diff(string, i) for i, _ in enumerate(string)])  # sum of diff with previous char
+    b: int = sum([get_prev_diff(string, i) for i, _ in enumerate(string)])  # sum of diff with previous char
+
+    # c := (a OR b) AND ((NOT a) shift left by 2 bits)
 
     return -1
 
