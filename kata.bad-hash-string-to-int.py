32153e0ecf04934c855bab0c801451164850454a
Yahya Jabary <jabaryyahya@gmail.com>
2023-11-22 16:34:02 -0500

    update

--- kata.bad-hash-string-to-int.py
+++ kata.bad-hash-string-to-int.py
@@ -2,13 +2,29 @@
 
 
 def hash_str(string: str) -> int:
-    a: int = sum([ord(char) for char in string])  # char sum
-    get_prev_diff = lambda string, i: ord(string[i - 1]) - ord(string[i]) if i != 0 else 0  # diff with previous char
-    b: int = sum([get_prev_diff(string, i) for i, _ in enumerate(string)])  # sum of diff with previous char
+    a: int = sum(ord(c) for c in string)
+    print(f"a = {a}")
 
-    # c := (a OR b) AND ((NOT a) shift left by 2 bits)
+    get_prev_diff = lambda string, i: ord(string[i]) - ord(string[i - 1]) if i > 0 else 0
+    b: int = sum(get_prev_diff(string, i) for i, _ in enumerate(string))  # sum of diff with previous char
+    b_simplified: int = sum(ord(b) - ord(a) for a, b in zip(string, string[1:]))
+    print(f"b = {b}")
 
-    return -1
+    c: int = (a | b) & (~a << 2)
+    print(f"c = {c}")
+
+    d: int = c ^ (32 * (string.count(" ") + 1))
+    print(f"d = {d}")
+
+    return d
 
 
-hash_str("peanuts")
+def hash_str_simplified(s):
+    a = sum(ord(c) for c in s)
+    b = sum(ord(b) - ord(a) for a, b in zip(s, s[1:]))
+    c = (a | b) & (~a << 2)
+    return c ^ (32 * (s.count(" ") + 1))
+
+
+print(hash_str("ca"))
+print(hash_str_simplified("ca"))
