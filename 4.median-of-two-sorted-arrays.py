77727e81d7e61efbf7330b048740fa72f4a6e596
Yahya Jabary <jabaryyahya@gmail.com>
2023-11-27 20:23:47 -0500

    update

--- 4.median-of-two-sorted-arrays.py
+++ 4.median-of-two-sorted-arrays.py
@@ -1,20 +1,22 @@
 from collections import deque
 
-nums1 = [1, 3]
-nums2 = [2]
+nums1 = [1, 2, 3, 5]
+nums2 = [4]
 
 # find median of sorted arrays
 merged = []
 deque1 = deque(nums1)
 deque2 = deque(nums2)
-is_empty = lambda d: len(d) == 0
 
-while is_empty(deque1) or is_empty(deque2):
-    winner = deque1 if is_empty(deque2) and deque1[0] < deque2[0] else deque2
+is_empty = lambda x: len(x) == 0
+is_even = lambda x: len(x) % 2 == 0
+get_median = lambda x: (x[len(x) // 2] + x[len(x) // 2 - 1]) / 2 if is_even(x) else x[len(x) // 2]
+
+while not is_empty(deque1) or not is_empty(deque2):
+    winner = deque1 if is_empty(deque2) or deque1[0] < deque2[0] else deque2
     winner_val = winner.popleft()
-    print(winner_val)
     merged.append(winner_val)
 
-# WIP .......
 
 print(merged)
+print(get_median(merged))
