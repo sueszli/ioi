14c841a6835f9f1146ee50a915652a590f070801
Yahya Jabary <jabaryyahya@gmail.com>
2023-12-06 18:50:35 -0500

    update

--- aoc2023.day3.part1.py
+++ aoc2023.day3.part1.py
@@ -14,55 +14,29 @@ INPUT = """
 lines = [line for line in INPUT.split("\n") if line]
 
 
-indices = set()
+word_start_indices = set()
 
 for iline, line in enumerate(lines):
     for ichar, char in enumerate(line):
-        top = iline - 1
-        mid = iline
-        bottom = iline + 1
-        is_valid_vert = lambda x: x > -1 and x < len(lines)
-
-        left = ichar - 1
-        center = ichar
-        right = ichar + 1
-        is_valid_hor = lambda x: x > -1 and x < len(line)
-
-        # get indices of all digits around this char
-        digit_neighbour_indices = [
-            [
-                (top, left) if is_valid_vert(top) and is_valid_hor(left) and lines[top][left].isdigit() else None,
-                (top, center) if is_valid_vert(top) and is_valid_hor(center) and lines[top][center].isdigit() else None,
-                (top, right) if is_valid_vert(top) and is_valid_hor(right) and lines[top][right].isdigit() else None,
-            ],
-            [
-                (mid, left) if is_valid_vert(mid) and is_valid_hor(left) and lines[mid][left].isdigit() else None,
-                (mid, center) if is_valid_vert(mid) and is_valid_hor(center) and lines[mid][center].isdigit() else None,  # self
-                (mid, right) if is_valid_vert(mid) and is_valid_hor(right) and lines[mid][right].isdigit() else None,
-            ],
-            [
-                (bottom, left) if is_valid_vert(bottom) and is_valid_hor(left) and lines[bottom][left].isdigit() else None,
-                (bottom, center) if is_valid_vert(bottom) and is_valid_hor(center) and lines[bottom][center].isdigit() else None,
-                (bottom, right) if is_valid_vert(bottom) and is_valid_hor(right) and lines[bottom][right].isdigit() else None,
-            ],
-        ]
-
         if char == "." or char.isdigit():
             continue
-        new_indices = [elem for row in digit_neighbour_indices for elem in row if elem is not None]
-        indices.update(new_indices)
 
-# group by row
-indices_by_row = {}
-for index in indices:
-    row = index[0]
-    if row not in indices_by_row:
-        indices_by_row[row] = []
-    indices_by_row[row].append(index[1])
+        in_bounds = lambda x, y: x > -1 and x < len(lines) and y > -1 and y < len(line)
+        is_digit = lambda x, y: lines[x][y].isdigit()
 
-# sort each row
-for row in indices_by_row:
-    indices_by_row[row] = sorted(indices_by_row[row])
+        neighbours = [
+            (iline - 1, ichar - 1),
+            (iline - 1, ichar),
+            (iline - 1, ichar + 1),
+            (iline, ichar - 1),
+            (iline, ichar),
+            (iline, ichar + 1),
+            (iline + 1, ichar - 1),
+            (iline + 1, ichar),
+            (iline + 1, ichar + 1),
+        ]
+        digit_neighbours = [(x, y) for x, y in neighbours if in_bounds(x, y) and is_digit(x, y)]
 
-# for each index in each group, find the starting point of word / numbers
-print(indices_by_row)
+        print(char, "--->", digit_neighbours)
+
+        # indices.update(new_indices)
