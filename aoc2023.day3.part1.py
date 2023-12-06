9420eb8238c20d9810ff0a398e16db833cbfd81d
Yahya Jabary <jabaryyahya@gmail.com>
2023-12-06 17:29:10 -0500

    update

--- aoc2023.day3.part1.py
+++ aoc2023.day3.part1.py
@@ -1,7 +1,3 @@
-from calendar import c
-from curses.ascii import isdigit
-
-
 INPUT = """
 467..114..
 ...*......
@@ -17,7 +13,8 @@ INPUT = """
 
 lines = [line for line in INPUT.split("\n") if line]
 
-output = []
+
+indices = set()
 
 for iline, line in enumerate(lines):
     for ichar, char in enumerate(line):
@@ -31,31 +28,41 @@ for iline, line in enumerate(lines):
         right = ichar + 1
         is_valid_hor = lambda x: x > -1 and x < len(line)
 
-        neighbour_indices = [
+        # get indices of all digits around this char
+        digit_neighbour_indices = [
             [
-                (top, left) if is_valid_vert(top) and is_valid_hor(left) else None,
-                (top, center) if is_valid_vert(top) and is_valid_hor(center) else None,
-                (top, right) if is_valid_vert(top) and is_valid_hor(right) else None,
+                (top, left) if is_valid_vert(top) and is_valid_hor(left) and lines[top][left].isdigit() else None,
+                (top, center) if is_valid_vert(top) and is_valid_hor(center) and lines[top][center].isdigit() else None,
+                (top, right) if is_valid_vert(top) and is_valid_hor(right) and lines[top][right].isdigit() else None,
             ],
             [
-                (mid, left) if is_valid_vert(mid) and is_valid_hor(left) else None,
-                (mid, center) if is_valid_vert(mid) and is_valid_hor(center) else None,  # self
-                (mid, right) if is_valid_vert(mid) and is_valid_hor(right) else None,
+                (mid, left) if is_valid_vert(mid) and is_valid_hor(left) and lines[mid][left].isdigit() else None,
+                (mid, center) if is_valid_vert(mid) and is_valid_hor(center) and lines[mid][center].isdigit() else None,  # self
+                (mid, right) if is_valid_vert(mid) and is_valid_hor(right) and lines[mid][right].isdigit() else None,
             ],
             [
-                (bottom, left) if is_valid_vert(bottom) and is_valid_hor(left) else None,
-                (bottom, center) if is_valid_vert(bottom) and is_valid_hor(center) else None,
-                (bottom, right) if is_valid_vert(bottom) and is_valid_hor(right) else None,
+                (bottom, left) if is_valid_vert(bottom) and is_valid_hor(left) and lines[bottom][left].isdigit() else None,
+                (bottom, center) if is_valid_vert(bottom) and is_valid_hor(center) and lines[bottom][center].isdigit() else None,
+                (bottom, right) if is_valid_vert(bottom) and is_valid_hor(right) and lines[bottom][right].isdigit() else None,
             ],
         ]
-        flat_neighbour_indices = [elem for row in neighbour_indices for elem in row]
 
-        # self must be symbol
-        if isdigit(char) or char == ".":
+        if char == "." or char.isdigit():
             continue
+        new_indices = [elem for row in digit_neighbour_indices for elem in row if elem is not None]
+        indices.update(new_indices)
 
-        # neighbour must be number
+# group by row
+indices_by_row = {}
+for index in indices:
+    row = index[0]
+    if row not in indices_by_row:
+        indices_by_row[row] = []
+    indices_by_row[row].append(index[1])
 
-        # get smallest number in neighbours
+# sort each row
+for row in indices_by_row:
+    indices_by_row[row] = sorted(indices_by_row[row])
 
-        print(char, "-->", flat_neighbour_indices)
+# for each index in each group, find the starting point of word / numbers
+print(indices_by_row)
