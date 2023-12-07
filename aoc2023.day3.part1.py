7edaa6775e47ca898745adbd09f97c5366e60c12
Yahya Jabary <jabaryyahya@gmail.com>
2023-12-06 19:42:44 -0500

    update

--- aoc2023.day3.part1.py
+++ aoc2023.day3.part1.py
@@ -15,13 +15,14 @@ lines = [line for line in INPUT.split("\n") if line]
 
 is_digit = lambda x, y: x > -1 and x < len(lines) and y > -1 and y < len(lines[x]) and lines[x][y].isdigit()
 
-num_start_indices = set()
+word_dict = {}  # key: start index, value: word
 
 for iline, line in enumerate(lines):
     for ichar, char in enumerate(line):
         if char == "." or char.isdigit():
             continue
 
+        # get all neighbouring digits
         neighbours = [
             (iline - 1, ichar - 1),
             (iline - 1, ichar),
@@ -36,25 +37,27 @@ for iline, line in enumerate(lines):
         digit_neighbours = [(x, y) for x, y in neighbours if is_digit(x, y)]
 
         print("processing", char)
-        print("\tdigit neighbours of", char, ":", digit_neighbours)
 
-        # get start of word
+        # get start of words
+        starts_indices = set()
         for x, y in digit_neighbours:
             curr = y
             while is_digit(x, curr):
                 curr -= 1
             print("\t\tword for", x, y, "starts at", x, curr + 1)
-            num_start_indices.add((x, curr + 1))
+            starts_indices.add((x, curr + 1))
 
-# get entire word
-nums = []
-for x, y in num_start_indices:
-    num = ""
-    curr = y
-    while is_digit(x, curr):
-        num += lines[x][curr]
-        curr += 1
-    nums.append(int(num))
+        # read words
+        words = []
+        for x, y in starts_indices:
+            word = ""
+            curr = y
+            while is_digit(x, curr):
+                word += lines[x][curr]
+                curr += 1
 
-print(nums)
-print("sum:", sum(nums))
+            words.append(word)
+            word_dict[(x, y)] = word
+
+total = sum([int(word) for word in list(word_dict.values())])
+print(f"total: {total}")
