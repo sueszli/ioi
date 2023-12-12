283fddde4845bcf737e11322670db93d47e69d1b
Yahya Jabary <jabaryyahya@gmail.com>
2023-12-11 21:28:35 -0500

    update

--- aoc2023.day6.part1.py
+++ aoc2023.day6.part1.py
@@ -1,18 +1,26 @@
+import math
+
 INPUT = """
-Time:      7  15   30
-Distance:  9  40  200
+Time:        40     92     97     90
+Distance:   215   1064   1505   1100
 """
 
-# my boat:
-# - starting speed: 0mm/ms
-# - each second holding the button = +1mm/ms
+# goal: beating maxdist
+# each second holding the button = +1mm/ms
 
 times = list(map(int, INPUT.strip().splitlines()[0].split(":")[1].split()))
 distances = list(map(int, INPUT.strip().splitlines()[1].split(":")[1].split()))
 assert len(times) == len(distances)
 races = list(zip(times, distances))
 
+ans = []
 for race in races:
     time = race[0]
-    dist = race[1]
-    print(time, dist)
+    maxdist = race[1]
+
+    is_solution = lambda x: (time - x) * x > maxdist  # time holding button beats maxdist
+    solutions = [x for x in range(0, time) if is_solution(x)]
+
+    ans.append(len(solutions))
+
+print("solution:", math.prod(ans))
