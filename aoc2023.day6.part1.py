970ad32f28d659afd8b5d9eb93263fddd89ad07b
Yahya Jabary <jabaryyahya@gmail.com>
2023-12-11 21:09:06 -0500

    update

--- aoc2023.day6.part1.py
+++ aoc2023.day6.part1.py
@@ -1,1 +1,18 @@
-print("LETS GOOOOOOOOO")
+INPUT = """
+Time:      7  15   30
+Distance:  9  40  200
+"""
+
+# my boat:
+# - starting speed: 0mm/ms
+# - each second holding the button = +1mm/ms
+
+times = list(map(int, INPUT.strip().splitlines()[0].split(":")[1].split()))
+distances = list(map(int, INPUT.strip().splitlines()[1].split(":")[1].split()))
+assert len(times) == len(distances)
+races = list(zip(times, distances))
+
+for race in races:
+    time = race[0]
+    dist = race[1]
+    print(time, dist)
