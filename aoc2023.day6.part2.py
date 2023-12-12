ddd89924d552790334f81b1208af955e9ff6a238
Yahya Jabary <jabaryyahya@gmail.com>
2023-12-11 21:42:31 -0500

    update

--- aoc2023.day6.part2.py
+++ aoc2023.day6.part2.py
@@ -1,11 +1,9 @@
-import math
-
 INPUT = """
 Time:        40     92     97     90
 Distance:   215   1064   1505   1100
 """
 
-time = int("".join(INPUT.strip().splitlines()[0].split(":")[1].split()))
-maxdist = int("".join(INPUT.strip().splitlines()[1].split(":")[1].split()))
-ans = len([x for x in range(0, time) if (time - x) * x > maxdist])
-print("solution:", ans)
+line = lambda x: int("".join(INPUT.strip().splitlines()[x].split(":")[1].split()))
+time = line(0)
+maxdist = line(1)
+print("solution:", len([x for x in range(0, time) if (time - x) * x > maxdist]))
