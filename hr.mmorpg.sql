4cc9b8b9501218661b7b66042e2ee646fe641137
Yahya Jabary <jabaryyahya@gmail.com>
2024-01-08 21:14:52 +0100

    update

---
+++ hr.mmorpg.sql
@@ -0,0 +1,17 @@
+/*
+Enter your query below.
+Please append a semicolon ";" at the end of the query
+*/
+
+SELECT
+    username,
+    email,
+    COUNT(items.id) as items,
+    SUM(items.weight) as total_weight
+FROM accounts
+JOIN accounts_items ON accounts.id = accounts_items.account_id /* accounts_items is a join table */
+JOIN items ON items.id = accounts_items.item_id
+GROUP BY username, email
+HAVING SUM(items.weight) > 20
+ORDER BY total_weight DESC, username ASC
+;
